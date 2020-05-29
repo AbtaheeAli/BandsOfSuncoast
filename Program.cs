47217def073b4b6e62a158472fd969f8af1db6a3
Abtahee Ali <abtaheeali@gmail.com>
2020-05-29 16:16:00 -0400

    Added variables bands, albums, and context

--- Program.cs
+++ Program.cs
@@ -8,7 +8,28 @@ namespace BandsOfSuncoast
     {
         static void Main(string[] args)
         {
+            var context = new BandsOfSuncoastContext();
 
+            var bands = context.Bands;
+
+            var albums = context.Albums.
+            Include(album => album.Band);
+
+            // var newBand = new Band
+            // {
+            //     Name = "The Owls",
+            //     CountryOfOrigin = "Australia",
+            //     NumberOfMembers = 5,
+            //     Website = "Owls.com",
+            //     Style = "R&B",
+            //     IsSigned = true,
+            //     ContactName = "Riz",
+            //     ContactPhoneNumber = "727-239-9058"
+            // };
+
+            // context.Bands.Add(newBand);
+            // context.SaveChanges();
+            // Console.WriteLine($"Here are the list of {bands}");
         }
     }
 }
