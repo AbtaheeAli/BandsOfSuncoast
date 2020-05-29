66ba0844eaf9f7ab56468ebb279856427112b7d6
Abtahee Ali <abtaheeali@gmail.com>
2020-05-29 16:14:55 -0400

    Added class BandsOfSuncoastContext

--- BandsOfSuncoastContext.cs
+++ BandsOfSuncoastContext.cs
@@ -7,7 +7,14 @@ namespace BandsOfSuncoast
 
     class BandsOfSuncoastContext : DbContext
     {
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Album> Albums { get; set; }
 
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastBands");
+            // var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            // optionsBuilder.UseLoggerFactory(loggerFactory);
+        }
     }
 }
