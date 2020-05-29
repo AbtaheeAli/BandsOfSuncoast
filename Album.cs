ac8442e96ee4ef5d5a9c8279070dadde5cc6eba1
Abtahee Ali <abtaheeali@gmail.com>
2020-05-29 16:14:12 -0400

    Added a class Album for albums

--- Album.cs
+++ Album.cs
@@ -6,6 +6,12 @@ namespace BandsOfSuncoast
 {
     class Album
     {
-
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public int MyProperty { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int BandId { get; set; }
+        public Band Band { get; set; }
     }
 }
